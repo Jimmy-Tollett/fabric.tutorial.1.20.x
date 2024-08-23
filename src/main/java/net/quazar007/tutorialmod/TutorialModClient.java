eee511734597c25ea4969faa003e95db1b5e824b
Jimmy Tollett <jimmy.tollett@all-saints.org>
2024-08-23 16:20:19 -0500

    Initial commit

---
+++ src/main/java/net/quazar007/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.quazar007.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
